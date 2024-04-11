ac5d0c59815a15df9d512b572b2e70eaec8cfd33
Rhino-ty <okko8522@gmail.com>
2024-04-11 15:41:03 +0900

    feat: 상품 리스트 구현 및 디자인 임시 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { Product } from './types/product.types';
 
 function App() {
-  return <div className='App'></div>;
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div className='loading'>Loading...</div>;
+
+  return (
+    <div className='App'>
+      <h1>Product List</h1>
+      <div className='products'>
+        {products?.map((product) => (
+          <div key={product?.id} className='product'>
+            <img src={product?.thumbnail} alt={product?.title} />
+            <div>
+              <div>{product?.brand}</div>
+              <div>{product?.title}</div>
+            </div>
+            <div>${product?.price}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
