8774f488d257a02da7872c8b44237c8435b3b7b9
Rhino-ty <okko8522@gmail.com>
2024-04-11 15:47:40 +0900

    feat: react-router-dom라이브러리 설치 후 라우팅을 사용하기 위해 App.tsx 라우팅 코드로 변경 및 pages 폴더로 소스 코드 이사

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { Product } from './types/product.types';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductListPage from './pages/product-list';
+import ProductDetailPage from './pages/product-detail';
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch('https://dummyjson.com/products')
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data.products);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error('Error fetching data: ', error);
-        setLoading(false);
-      });
-  }, []);
-
-  if (loading) return <div className='loading'>Loading...</div>;
-
   return (
-    <div className='App'>
-      <h1>Product List</h1>
-      <div className='products'>
-        {products?.map((product) => (
-          <div key={product?.id} className='product'>
-            <img src={product?.thumbnail} alt={product?.title} />
-            <div>
-              <div>{product?.brand}</div>
-              <div>{product?.title}</div>
-            </div>
-            <div>${product?.price}</div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<ProductListPage />} />
+        <Route path='/product/:productId' element={<ProductDetailPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/product-list/index.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from 'react';
+import { Product } from '../../types/product.types';
+import './index.css';
+
+export default function ProductListPage() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data.products);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Error fetching data: ', error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <div className='loading'>Loading...</div>;
+
+  return (
+    <main className='main-contatiner'>
+      <h1>Product List</h1>
+      <div className='products'>
+        {products.map((product) => (
+          <div key={product?.id} className='product'>
+            <img src={product?.thumbnail} alt={product?.title} />
+            <div>
+              <div>{product?.brand}</div>
+              <div>{product?.title}</div>
+            </div>
+            <div>${product?.price}</div>
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
